e0c9aec0b9460bb4b8a14017f2890006b3cd5388
Carlo Castillo <carlo89512@yahoo.com>
2025-04-12 20:11:44 -0700

    updated bunch of things

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,37 +1,50 @@
-export const cart = [
-    {
-      productId: '43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2,
-    },
-    {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
     }
-  ];
-  
-  // Example add-to-cart function snippet (ensure matchingItem is defined)
-  // let matchingItem = cart.find(item => item.productId === productId);
-  // if (matchingItem) {
-  //   matchingItem.quantity += 1;
-  // } else {
-  //   cart.push({
-  //     productId: productId,
-  //     quantity: 1,
-  //   });
-  // }
-  
-  export function removeFromCart(productId) {
-    const newCart = [];
-  
-    // Use a proper parameter in the forEach loop so that 'item' is defined.
-    cart.forEach((item) => {
-      if (productId !== item.productId) {
-        newCart.push(item);
-      }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
     });
-  
-    // Clear the original cart array (mutating the const cart).
-    cart.length = 0;
-    newCart.forEach((item) => cart.push(item));
   }
-  
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
