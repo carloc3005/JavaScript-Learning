42d66be412b6723e55193c3f83739a448ef1939c
Carlo Castillo <carlo89512@yahoo.com>
2025-04-16 18:39:29 -0700

    trying to test

--- javascript-amazon-project-main/test-jasmine/data/cartTest.js
+++ javascript-amazon-project-main/test-jasmine/data/cartTest.js
@@ -1,5 +1,12 @@
-import {addToCart} from '../../data/cart.js';
+import {addToCart, cart} from '../../data/cart.js';
 
 describe('Test Suite: addToCart', () => {
-	it('')
+	it('adds an existing product to the cart', () => {
+
+	});
+
+	it('adds a new product to the cart', () => {
+		addtoCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+		expect('cart.length').toEqual(1);
+	});
 });
